a45ca1db0b874784ca9e916e7dfa8035951706a1
MACsepehri <macsepehri@gmail.com>
2026-07-22 18:58:16 +0330

    Added font and small_font to game

---
+++ SpaceProgramProjectAssets/config.py
@@ -0,0 +1,6 @@
+import pygame
+
+# init
+pygame.init()
+font = pygame.font.Font("SpaceProgramProjectAssets/file/font/main.ttf", 48)
+small_font = pygame.font.Font("SpaceProgramProjectAssets/file/font/main.ttf", 32)
