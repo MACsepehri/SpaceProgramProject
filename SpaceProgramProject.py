4f56182068a7ea4748a845cadc17c7868b6c421d
MACsepehri <macsepehri@gmail.com>
2026-07-22 19:35:18 +0330

    Added new functions.

--- SpaceProgramProject.py
+++ SpaceProgramProject.py
@@ -9,7 +9,14 @@ width, height = display_info.current_w, display_info.current_h
 win = pygame.display.set_mode((width, height))
 status = "menu"
 
-# menu button
+# actions
+def start():
+    global status
+
+    status = "ingame"
+
+
+# draw button
 def draw_menu():
     global status
 
@@ -20,11 +27,24 @@ def draw_menu():
             config.Button(win, 0, 450, 200, 90, config.font, "Exit", middle=True)
         ]
         config.draw_button(btn)
-        config.button_action(btn, [lambda: ..., lambda: ..., sys.exit])
+        config.button_action(btn, [start, lambda: ..., sys.exit])
+
+def draw_ingame_button():
+    global status
+
+    if status == "ingame":
+        btn = [
+            config.Button(win, 30, 250, 200, 90, config.font, "Rocket 1"),
+            config.Button(win, 30, 350, 200, 90, config.font, "Rocket 2"),
+            config.Button(win, 30, 450, 200, 90, config.font, "Rocket 3")
+        ]
+        config.draw_button(btn)
+        config.button_action(btn, [lambda: ..., lambda: ..., lambda: ...])
 
 # update
 def update():
     draw_menu()
+    draw_ingame_button()
 
 # run
 def main():
