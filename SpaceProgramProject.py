7c55d45bf878a88466323fb7f9723b73a8d34613
MACsepehri <macsepehri@gmail.com>
2026-07-22 19:25:35 +0330

    Added draw_menu() function.

--- SpaceProgramProject.py
+++ SpaceProgramProject.py
@@ -7,10 +7,24 @@ pygame.init()
 display_info = pygame.display.Info()
 width, height = display_info.current_w, display_info.current_h
 win = pygame.display.set_mode((width, height))
+status = "menu"
+
+# menu button
+def draw_menu():
+    global status
+
+    if status == "menu":
+        btn = [
+            config.Button(win, 0, 250, 200, 90, config.font, "Start", middle=True),
+            config.Button(win, 0, 350, 200, 90, config.font, "Setting", middle=True),
+            config.Button(win, 0, 450, 200, 90, config.font, "Exit", middle=True)
+        ]
+        config.draw_button(btn)
+        config.button_action(btn, [lambda: ..., lambda: ..., sys.exit])
 
 # update
 def update():
-    pass
+    draw_menu()
 
 # run
 def main():
