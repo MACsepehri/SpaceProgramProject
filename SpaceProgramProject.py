73915be8986f5950722df08d0ad1aaf1145d4ba0
MACsepehri <macsepehri@gmail.com>
2026-07-22 19:01:12 +0330

    Added update() function.

--- SpaceProgramProject.py
+++ SpaceProgramProject.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from SpaceProgramProjectAssets import config
 
 # init
 pygame.init()
@@ -7,12 +8,20 @@ display_info = pygame.display.Info()
 width, height = display_info.current_w, display_info.current_h
 win = pygame.display.set_mode((width, height))
 
+# update
+def update():
+    pass
+
 # run
 def main():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
+
+        win.fill("skyblue")
+        update()
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
